d81d190fcd7ea838dd03e7c05bf16d1e11efb3a1
aki09 <akshatbhatia05@gmail.com>
2021-05-06 19:50:10 +0530

    sql inventory

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const bodyParser = require("body-parser");
 const app = express();
 var items = [];
 
-/* var connection = mysql.createConnection({
+var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'nishtha',
 	database: 'dbdb',
 	port: 3306
-}); */
+}); 
 
-/* connection.connect((err) => {
+connection.connect((err) => {
 	if (err) {
 		console.log("ERRE")
 	}
@@ -21,7 +21,7 @@ var items = [];
 		console.log("HELLOO NISHTHAAA")
 	}
 });
- */
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -38,48 +38,49 @@ app.get('/index.html', function (req, res) {
 	res.render('index', { new_item: newItem });
 });
 
-app.get('/category.html', function (req, res) {
-	res.render('category');
+// app.get('/category.html', function (req, res) {
+// 	res.render('category');
+// });
+
+// app.get('/category.html', (req, res) => {
+// 	let sql = 'select * from Inventory';
+// 	connection.query(sql, (err, rows) => {
+// 		if (err) {
+// 			throw err
+// 		} else {
+// 			console.log("DATA BHEJ DIYA MENE NISHTHA")
+// 			// console.log(rows)
+// 			console.log("DONT SCOLD ME")
+// 			res.send("Done");
+// 		}
+// 		console.log(rows[0]['Type']);
+// 	})
+
+// 	// console.log(post)
+// });
+
+app.get('/category.html', (req, res) => {
+	let sql = 'select * from Inventory';
+	connection.query(sql, (err, rows) => {
+		if (err) {
+			throw err
+		} else {
+			console.log("DATA BHEJ DIYA MENE NISHTHA")
+			// console.log(rows)
+			console.log("DONT SCOLD ME")
+			// res.send("Done");
+		}
+		console.log(rows[0]['Type']);
+
+		for (let i = 0; i < rows.length; i++) {
+			items.push(rows[i]);
+		}
+		res.render('category', { inventory: items });
+		console.log(items);
+	})
+
+	// console.log(post)
 });
-
-// app.get('/category.html', (req, res) => {
-// 	let sql = 'select * from Inventory';
-// 	connection.query(sql, (err, rows) => {
-// 		if (err) {
-// 			throw err
-// 		} else {
-// 			console.log("DATA BHEJ DIYA MENE NISHTHA")
-// 			// console.log(rows)
-// 			console.log("DONT SCOLD ME")
-// 			res.send("Done");
-// 		}
-// 		console.log(rows[0]['Type']);
-// 	})
-
-// 	// console.log(post)
-// });
-
-// app.get('/category.html', (req, res) => {
-// 	let sql = 'select * from Inventory';
-// 	connection.query(sql, (err, rows) => {
-// 		if (err) {
-// 			throw err
-// 		} else {
-// 			console.log("DATA BHEJ DIYA MENE NISHTHA")
-// 			// console.log(rows)
-// 			console.log("DONT SCOLD ME")
-// 			res.send("Done");
-// 		}
-// 		console.log(rows[0]['Type']);
-
-// 		for (let i = 0; i < rows.length; i++) {
-// 			items.push(rows[i]);
-// 		}
-// 		console.log(items);
-// 	})
-
-// 	// console.log(post)
-// });
 
 app.listen(5000, function () {
 	console.log("Started");
