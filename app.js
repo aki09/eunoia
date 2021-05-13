c31477865c9bf48e4a3f52778b22238965f00848
aki09 <akshatbhatia05@gmail.com>
2021-05-14 01:21:48 +0530

    order server

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 var items = [];
 var customers = [];
 
-/* var connection = mysql.createConnection({
+var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'nishtha',
@@ -21,7 +21,7 @@ connection.connect((err) => {
 	else {
 		console.log("HELLOO NISHTHAAA")
 	}
-}); */
+});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -161,6 +161,62 @@ app.post('/ad-customer.html', (req, res) => {
 	res.redirect('/ad-customer.html');
 	// console.log(post)
 });
+
+app.post('/ad-orders.html', (req, res) => {
+	var oid = req.body.oid;
+	var odate = req.body.odate;
+	var add = req.body.add;
+	var cid = req.body.cid;
+	var iid = req.body.iid;
+	var amt = req.body.amt;
+	var paymode = req.body.paymode;
+	var pdate = req.body.pdate;
+	var status = 'Pending';
+	var sql = "insert into orders set ?"
+	let o_post = { Order_ID: oid, Order_Date: odate, Amount: amt, Delivery_Address: add, Order_Status: status }
+	let ol_post = { Order_ID: oid, Item_ID: iid }
+	let co_post = { Customer_ID: cid, Order_ID: oid }
+	let pay_post = { Order_ID: oid, Amount: amt, Payment_Mode: paymode, Payment_Date: pdate }
+
+	connection.query(sql, o_post, (err, rows) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log(rows);
+		}
+	});
+
+	var sql = "insert into order_list set ?"
+
+	connection.query(sql, ol_post, (err, rows) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log(rows);
+		}
+	});
+
+	var sql = "insert into customer_order set ?"
+
+	connection.query(sql, co_post, (err, rows) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log(rows);
+		}
+	});
+
+	var sql = "insert into payment set ?"
+
+	connection.query(sql, pay_post, (err, rows) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log(rows);
+		}
+	});
+	res.redirect('/ad-orders.html');
+})
 
 app.listen(5000, function () {
 	console.log("Started");
